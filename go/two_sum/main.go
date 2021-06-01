49bd6037931198739990189ec75675739e003283
Yaroslav Kostyannikov <ykostyannikov@lohika.com>
2021-06-01 11:00:39 +0300

    fmt and lint

--- go/two_sum/main.go
+++ go/two_sum/main.go
@@ -1,18 +1,16 @@
 package main
 
-import "fmt"
+func main(nums []int, target int) []int {
 
-func main() {
-
-    var result []int
-    main:
-    for i := range nums {
-        for j := range nums {
-            if i < j && nums[i] + nums[j] == target {
-                result = append(result, i, j)
-                break main
-            }
-        }
-    }
-    return result
+	var result []int
+main:
+	for i := range nums {
+		for j := range nums {
+			if i < j && nums[i]+nums[j] == target {
+				result = append(result, i, j)
+				break main
+			}
+		}
+	}
+	return result
 }
