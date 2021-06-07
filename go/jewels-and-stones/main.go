8d316638e2b53336f79bdae384a8b3e38fedbe52
Yaroslav Kostyannikov <ykostyannikov@lohika.com>
2021-06-07 04:58:57 +0300

    added solution for the jewels-and-stones

--- go/jewels-and-stones/main.go
+++ go/jewels-and-stones/main.go
@@ -1,1 +1,15 @@
 package main
+
+func main(jewels string, stones string) int {
+	var jewelsCounter int
+
+	for i, iv := range jewels {
+		for j, jv := range stones {
+			if iv == jv {
+				jewelsCounter += 1
+			}
+		}
+	}
+
+	return jewelsCounter
+}
