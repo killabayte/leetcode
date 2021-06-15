6f50761382e5d4568645e795c098a4bc3c8b9fa8
Yaroslav Kostiannikov <ykostyannikov@lohika.com>
2021-06-15 22:36:00 +0300

    added constraints

--- go/longest-common-prefix/main.go
+++ go/longest-common-prefix/main.go
@@ -12,6 +12,11 @@ Example 2:
 Input: strs = ["dog","racecar","car"]
 Output: ""
 Explanation: There is no common prefix among the input strings.
+
+Constraints:
+  1 <= strs.length <= 200
+  0 <= strs[i].length <= 200
+  strs[i] consists of only lower-case English letters.
 */
 
 package main
