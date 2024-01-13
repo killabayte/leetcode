3295607eedfe796905c40606e9d2ffe34e2e9d9d
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-13 11:24:03 +0100

    LeetCode: Golang top-150 tasks

--- go/merge-sorted-array/main.go
+++ go/merge-sorted-array/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func merge(nums1 []int, m int, nums2 []int, n int) {
 	i, j, k := m-1, n-1, len(nums1)-1
 	for i >= 0 && j >= 0 {
@@ -17,4 +19,15 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 		j--
 		k--
 	}
+}
+
+func main() {
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	m := 3
+	nums2 := []int{2, 5, 6}
+	n := 3
+
+	merge(nums1, m, nums2, n)
+
+	fmt.Println(nums1)
 }
