07d8c92883bf3a25558d40bb3ac8ee9eccabd8ee
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-14 11:23:58 +0100

    LeetCode: Golang top-150 tasks

--- go/remove-dublicates-from-sorted-array/main.go
+++ go/remove-dublicates-from-sorted-array/main.go
@@ -1,1 +1,14 @@
 package main
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	uniqueIndex := 0
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[uniqueIndex] {
+			uniqueIndex++
+			nums[uniqueIndex] = nums[i]
+		}
+	}
+}
