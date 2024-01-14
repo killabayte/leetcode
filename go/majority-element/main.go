a463c28e356aeffc7b69ab5e9a3a8afd8351c6cd
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-14 21:48:51 +0100

    LeetCode: Golang top-150 tasks

--- go/majority-element/main.go
+++ go/majority-element/main.go
@@ -3,17 +3,30 @@ package main
 import "fmt"
 
 func majorityElement(nums []int) int {
-	majorityCount := len(nums) / 2
+	candidate := nums[0]
+	count := 1
 
-	recorded := make(map[int]int)
-
-	for i := 0; i < len(nums); i++ {
-		num := nums[i]
-		recorded[num]++
-
-		if count := recorded[num]; count > majorityCount {
-			return num
+	for i := 1; i < len(nums); i++ {
+		if count == 0 {
+			candidate = nums[i]
 		}
+
+		if nums[i] == candidate {
+			count++
+		} else {
+			count--
+		}
+	}
+
+	count = 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == candidate {
+			count++
+		}
+	}
+
+	if count > len(nums)/2 {
+		return candidate
 	}
 
 	return -1
