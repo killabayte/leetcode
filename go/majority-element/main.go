3572fb435c1d0f160ced6581b74a914505c92aec
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-14 21:35:35 +0100

    LeetCode: Golang top-150 tasks

--- go/majority-element/main.go
+++ go/majority-element/main.go
@@ -3,23 +3,37 @@ package main
 import "fmt"
 
 func majorityElement(nums []int) int {
-	numElement := 0
-	middleEntry := 0
-	for i := 0; i < len(nums); i++ {
-		if i == 0 {
-			numElement = nums[i]
-			middleEntry++
+	candidate := nums[0]
+	count := 1
+
+	for i := 1; i < len(nums); i++ {
+		if count == 0 {
+			candidate = nums[i]
+		}
+
+		if nums[i] == candidate {
+			count++
 		} else {
-			if nums[i] == numElement {
-				middleEntry++
-			}
+			count--
 		}
 	}
-	return middleEntry
+
+	count = 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == candidate {
+			count++
+		}
+	}
+
+	if count > len(nums)/2 {
+		return candidate
+	}
+
+	return -1
 }
 
 func main() {
-	nums := []int{2, 2, 1, 1, 1, 2, 2}
+	nums := []int{2, 2, 1, 1, 1, 1, 1, 2, 2}
 	result := majorityElement(nums)
 
 	fmt.Printf("Majority element: %d\n", result)
