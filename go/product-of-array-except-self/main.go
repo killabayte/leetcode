16374829e5fcce6d5d9bb341bdb288cd9ff59d97
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-18 16:47:22 +0100

    LeetCode: Golang top-150 tasks

--- go/product-of-array-except-self/main.go
+++ go/product-of-array-except-self/main.go
@@ -1,6 +1,13 @@
 package main
 
+import "fmt"
+
 func productExceptSelf(nums []int) []int {
 	ans := make([]int, len(nums))
-	for 
+
+	for _, i := range nums[1:] {
+		ans = append(ans, ans[-1]*nums[i-1])
+	}
+	fmt.Println(ans)
+	return ans
 }
