07f67c4eadc5fe181621cdf92b275a62f8c2b7e1
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-18 16:15:50 +0100

    LeetCode: Golang top-150 tasks

--- go/best-time-to-buy-and-sell-stock/main.go
+++ go/best-time-to-buy-and-sell-stock/main.go
@@ -1,6 +1,25 @@
 package main
 
 func maxProfit(prices []int) int {
-	min_price := 0
-	max_profit := 0
+	if len(prices) == 0 {
+		return 0
+	}
+
+	minPrice := prices[0]
+	maxProfit := 0
+
+	for _, price := range prices {
+		if price < minPrice {
+			minPrice = price
+		} else if price-minPrice > maxProfit {
+			maxProfit = price - minPrice
+		}
+	}
+
+	return maxProfit
+}
+
+func main() {
+	prices := []int{7, 1, 5, 3, 6, 4}
+	maxProfit(prices)
 }
