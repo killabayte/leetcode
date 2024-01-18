44188e51b6c5f7a51aed8abb0cbfbc219fb59123
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-18 17:14:03 +0100

    LeetCode: Golang top-150 tasks

--- go/product-of-array-except-self/main.go
+++ go/product-of-array-except-self/main.go
@@ -3,11 +3,37 @@ package main
 import "fmt"
 
 func productExceptSelf(nums []int) []int {
-	ans := make([]int, len(nums))
+	length := len(nums)
 
-	for _, i := range nums[1:] {
-		ans = append(ans, ans[-1]*nums[i-1])
+	// Initialize prefix and suffix arrays
+	prefix := make([]int, length)
+	suffix := make([]int, length)
+
+	// Calculate prefix products
+	prefixProduct := 1
+	for i := 0; i < length; i++ {
+		prefix[i] = prefixProduct
+		prefixProduct *= nums[i]
 	}
-	fmt.Println(ans)
-	return ans
+
+	// Calculate suffix products
+	suffixProduct := 1
+	for i := length - 1; i >= 0; i-- {
+		suffix[i] = suffixProduct
+		suffixProduct *= nums[i]
+	}
+
+	// Calculate the result array
+	result := make([]int, length)
+	for i := 0; i < length; i++ {
+		result[i] = prefix[i] * suffix[i]
+	}
+
+	return result
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4}
+	result := productExceptSelf(nums)
+	fmt.Println(result)
 }
