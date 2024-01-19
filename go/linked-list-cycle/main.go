c111197bbe67c8726441f5a816e92b9fb595a569
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-19 17:47:58 +0100

    LeetCode: Golang top-150 tasks

--- go/linked-list-cycle/main.go
+++ go/linked-list-cycle/main.go
@@ -1,11 +1,13 @@
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
-func hashCycle(head *ListNode) bool {
+func hasCycle(head *ListNode) bool {
 	slow, fast := head, head
 	for fast != nil && fast.Next != nil {
 		slow = slow.Next
@@ -17,4 +19,21 @@ func hashCycle(head *ListNode) bool {
 	}
 
 	return false
+}
+
+func main() {
+	list1 := &ListNode{Val: 1}
+	list1.Next = &ListNode{Val: 2}
+	list1.Next.Next = &ListNode{Val: 3}
+
+	result1 := hasCycle(list1)
+	fmt.Println("Example 1:", result1)
+
+	list2 := &ListNode{Val: 1}
+	list2.Next = &ListNode{Val: 2}
+	list2.Next.Next = &ListNode{Val: 3}
+	list2.Next.Next.Next = list2.Next
+
+	result2 := hasCycle(list2)
+	fmt.Println("Example 2:", result2)
 }
