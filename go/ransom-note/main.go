eba80ebc1b849f508be351fb73c7f6c3aa75d0c7
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-21 18:35:04 +0100

    LeetCode: Golang top-150 tasks

--- go/ransom-note/main.go
+++ go/ransom-note/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func canConstruct(ransomNote string, magazine string) bool {
 	charCount := make(map[byte]int)
 
@@ -19,5 +21,15 @@ func canConstruct(ransomNote string, magazine string) bool {
 }
 
 func main() {
+	// Example 1
+	ransomNote1, magazine1 := "a", "b"
+	fmt.Println(canConstruct(ransomNote1, magazine1)) // Output: false
 
+	// Example 2
+	ransomNote2, magazine2 := "aa", "ab"
+	fmt.Println(canConstruct(ransomNote2, magazine2)) // Output: false
+
+	// Example 3
+	ransomNote3, magazine3 := "aa", "aab"
+	fmt.Println(canConstruct(ransomNote3, magazine3)) // Output: true
 }
