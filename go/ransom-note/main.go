c505fb0f53d07af821115f7ba57e6606f7682bb3
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-21 18:34:25 +0100

    LeetCode: Golang top-150 tasks

--- go/ransom-note/main.go
+++ go/ransom-note/main.go
@@ -1,6 +1,20 @@
 package main
 
 func canConstruct(ransomNote string, magazine string) bool {
-	// TODO
-	return false
+	charCount := make(map[byte]int)
+
+	for i := 0; i < len(magazine); i++ {
+		charCount[magazine[i]]++
+	}
+
+	// Check if ransomNote can be constructed
+	for i := 0; i < len(ransomNote); i++ {
+		char := ransomNote[i]
+		if charCount[char] <= 0 {
+			return false
+		}
+		charCount[char]--
+	}
+
+	return true
 }
