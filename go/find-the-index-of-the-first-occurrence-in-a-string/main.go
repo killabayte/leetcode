42a16f9db9610f6a9da3649a12d8d22f8a39584f
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-27 11:05:18 +0100

    LeetCode: Golang top-150 tasks

--- go/find-the-index-of-the-first-occurrence-in-a-string/main.go
+++ go/find-the-index-of-the-first-occurrence-in-a-string/main.go
@@ -1,17 +1,20 @@
 package main
 
-import "strings"
-
 func strStr(haystack string, needle string) int {
+	// Если игла (needle) пуста, вернуть 0
 	if needle == "" {
-		return -1
-	}
-
-	result := strings.Contains(haystack, needle)
-	if result {
 		return 0
 	}
-	return -1
+
+	// Проход по строке (хейстеку) с использованием окна размером len(needle)
+	for i := 0; i <= len(haystack)-len(needle); i++ {
+		// Проверка совпадения подстроки (иглы) с текущим окном
+		if haystack[i:i+len(needle)] == needle {
+			return i // Возвращаем индекс первого вхождения
+		}
+	}
+
+	return -1 // Игла не найдена в хейстеке
 }
 
 func main() {
