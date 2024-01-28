08544917d29efd20025fadf1f86de3245d749f3f
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-28 17:42:19 +0100

    LeetCode: Golang top-150 tasks

--- python/ransome-note/main.py
+++ python/ransome-note/main.py
@@ -3,3 +3,8 @@ def canConstruct(self, ransomNote: str, magazine: str) -> bool:
 
     for char in magazine:
         letterCount[char] += 1
+
+    for char in ransomNote:
+        letterCount[char] -= 1
+        if letterCount[char] < 0:
+            return False
