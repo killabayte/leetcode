6dc212c9b09ba67f16e667e8eee2331ac6c22298
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-28 16:46:32 +0100

    LeetCode: Golang top-150 tasks

--- python/valid-palidrome/main.py
+++ python/valid-palidrome/main.py
@@ -7,9 +7,14 @@ def isPalindrome(s: str) -> bool:
             return False
         left += 1
         right -= 1
+
+    return True
     
 def cleanString(s: str) -> str:
     return ''.join([c.lower() for c in s if isAlphaNumeric(c)])
 
 def isAlphaNumeric(c: str) -> bool:
     return c.isalnum()
+
+print(isPalindrome("A man, a plan, a canal: Panama"))
+print(isPalindrome("race a car"))
