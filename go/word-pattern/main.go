2fcf33f9f5b8eaa2f4e587d9d797272bf365912e
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-28 10:32:50 +0100

    LeetCode: Golang top-150 tasks

--- go/word-pattern/main.go
+++ go/word-pattern/main.go
@@ -1,6 +1,12 @@
 package main
 
+import "strings"
+
 func wordPattern(pattern string, s string) bool {
 	controlMap := make(map[string]string)
+
+	if len(pattern) != len(strings.Split(s, " ")) {
+		return false
+	}
 
 }
