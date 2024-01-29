4a4f0391ed7734887c9bd8574e0b36e7e01f9590
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2024-01-29 10:36:28 +0100

    LeetCode: Golang top-150 tasks

--- python/summary-ranges/main.py
+++ python/summary-ranges/main.py
@@ -4,4 +4,16 @@ def summaryRanges(nums: List[int]) -> List[str]:
     for i in range(len(nums)):
         start = nums[i]
         end = start
-        
+
+        while i+1 < len(nums) and nums[i+1] == nums[i]+1:
+            end = nums[i+1]
+            i += 1
+
+        if start == end:
+            result.append(str(start))
+        else:
+            result.append(str(start) + "->" + str(end))
+
+    return result
+
+print(summaryRanges([0,1,2,4,5,7]))
